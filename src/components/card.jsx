4fee6a5477978e2de44706e45caf92b411145d24
Ankit-69k <ankitkumarray19@gmail.com>
2024-05-11 20:26:32 +0530

    first commit

---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='border bg-red-500'>
+      <h1>Name: Mrityunjoy</h1>
+      <h1>Department: IT</h1>
+      <h1>College: GU</h1>
+    </div>
+  )
+}
+
+export default Card
